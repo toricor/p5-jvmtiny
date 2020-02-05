49cca52e33a89c15104cc1ac332b45e34269e067
toricor <irotoridoritoriyaben@yahoo.co.jp>
2020-02-05 23:03:50 +0900

    Impl If*

---
+++ lib/Opcode/Ifge.pm
@@ -0,0 +1,62 @@
+package Opcode::Ifge;
+use warnings;
+use strict;
+use utf8;
+
+use Mouse;
+
+our $opcode = '9c';
+
+has operands => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    default  => sub {[]}
+);
+
+has operand_stack => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+has local_variables => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+has current_control_code_index => (
+    is       => 'rw',
+    isa      => 'Int',
+    required => 1,
+);
+
+has current_control_opcode_index => (
+    is       => 'rw',
+    isa      => 'Int',
+    required => 1,
+);
+
+sub run {
+    my ($self, $constant_pool_entries) = @_;
+    my $branchbyte2 = pop @{$self->operands};
+    my $branchbyte1 = pop @{$self->operands};
+    my $value = hex(pop @{$self->operand_stack});
+    return unless ($value >= 0);
+
+    $self->current_control_code_index($self->current_control_opcode_index + $self->_branch_offset($branchbyte1, $branchbyte2));
+}
+
+sub _branch_offset {
+    my ($self, $branch_byte1, $branch_byte2) = @_;
+    {
+        no warnings 'pack';
+        my $offset = unpack("c", pack("c", (hex($branch_byte1) << 8) | hex($branch_byte2)));
+        return $offset;
+    }
+}
+
+no Mouse;
+__PACKAGE__->meta->make_immutable;
+
+1;
